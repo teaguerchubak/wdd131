9a47484358b364a412eedf9ba38013a7e2e35f9b
teaguerchubak <teaguechubak@gmail.com>
2024-05-30 04:28:54 -0600

    bom2 assignment

---
+++ week05/scripts/bom.js
@@ -0,0 +1,78 @@
+    const input = document.querySelector('#favchap');
+    const button = document.querySelector('button');
+    const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+input.value = '';
+input.focus();
+
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    li.append(deletebutton);
+    list.append(li);
+
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length -1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+
+
+/*
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+    }
+    else input.focus();
+});
+*/
+

0083d58cc9b4305b782b93e8a1b23396030bb2a3
teaguerchubak <teaguechubak@gmail.com>
2024-05-30 06:55:31 -0600

    added js

---
+++ scripts/form.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: fc-1888,
+        name: "flux-capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: fc-2050,
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: fs-1987,
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: ac-2000,
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: jj-1969,
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+const productName = document.getElementById("product-name");
+
+products.forEach(item => {
+    const option = document.createElement("option");
+    option.value = item.id;
+    option.textContent = item.name;
+    productName.appendChild(option);
+});
+
+

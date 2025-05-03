bdf8b10c0d5d46353b479e37b0622da73f1345bb
teaguerchubak <teaguechubak@gmail.com>
2025-05-03 03:39:01 -0600

    added dark theme

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const bodyClass = document.getElementById("body");
+const themeSelector = document.getElementById("theme-select");
+
+themeSelector.addEventListener("change", () => {
+    const selection = themeSelector.value;
+    
+    if (selection === "light") {
+        bodyClass.classList.remove("dark")
+        document.getElementById("logo").src = "logo.webp"
+    }
+    else if (selection === "dark") {
+        bodyClass.classList.add("dark");
+        document.getElementById("logo").src = "logo-dark.png"
+    }
+});

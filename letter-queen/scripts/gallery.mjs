6f2a4ada2c98e7006a685d854a7e70a1849a93d6
teaguerchubak <teaguechubak@gmail.com>
2025-07-17 19:26:03 -0600

    final project start

---
+++ letter-queen/scripts/gallery.mjs
@@ -0,0 +1,83 @@
+const artwork = [
+    {   title: "Battle Royale",
+        year: 2022,
+        type: "Flourish", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-battle-royale.webp"
+    },
+    {   title: "Crawling Panther",
+        year: 2023,
+        type: "Flourish", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-crawling-panther.webp"
+    },
+    {   title: "Initial A",
+        year: 2017,
+        type: "Print", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-initial-a.webp"
+    },
+    {   title: "Letter Queen Bird",
+        year: 2024,
+        type: "Flourish", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-letterqueen-bird-flourish.webp"
+    },
+    {   title: "Tattooing by Luis Keys",
+        year: 2017,
+        type: "Business Card", 
+        medium: [
+            "ink"
+        ],
+        source: "./images/art-luis-keys-card.webp"
+    },
+    {   title: "Salt Lake Masonic Temple",
+        year: 2020,
+        type: "Window Sign", 
+        medium: [
+            "paint",
+            "gold leaf"
+        ],
+        source: "./images/art-masonic-temple.webp"
+    },
+    {   title: "SJM Monogram",
+        year: 2024,
+        type: "Engraving", 
+        medium: [
+            "copper"
+        ],
+        source: "./images/art-SJM-monogram.webp"
+    },
+    {   title: "Skull Flourish",
+        year: 2023,
+        type: "Flourish", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-skull-flourish.webp"
+    },
+    {   title: "The Alphabet",
+        year: 2016,
+        type: "Print", 
+        medium: [
+            "ink",
+            "paper"
+        ],
+        source: "./images/art-the-alphabet.webp"
+    }
+];
+
+export default artwork;
---
+++ letter-queen/scripts/main.js
@@ -0,0 +1,86 @@
+import artwork from "./gallery.mjs";
+
+
+/* FOOTER ICON CHANGES */
+const cartelBox = document.getElementById("cartel-box");
+const cartel = document.getElementById("cartel");
+const instaBox = document.getElementById("insta-box");
+const insta = document.getElementById("insta");
+
+cartelBox.addEventListener("mouseover", () => {
+    cartel.src = "images/icon-big-cartel-white.png";
+});
+
+cartelBox.addEventListener("mouseout", () => {
+    cartel.src = "images/icon-big-cartel.png";
+});
+
+instaBox.addEventListener("mouseover", () => {
+    insta.src = "images/icon-instagram-white.png";
+});
+
+instaBox.addEventListener("mouseout", () => {
+    insta.src = "images/icon-instagram.png";
+});
+
+
+/* GALLERY CARD CREATION */
+function tagsTemplate(tags) {
+    return tags.map(tag => `<p>${tag}</p>`).join(' ');
+}
+
+function artworkTemplate(item) {
+    return `
+        <div class="artwork-container">
+            <img class="art-img" src="${item.source}">
+            <div class="artwork-text">
+                <h2 class="title">${item.title}</h2>
+                <p class="year">${item.year}</p>
+                <p class="type">${item.type}</p>
+                <div class="tags">
+                    ${tagsTemplate(item.medium)}
+                </div>
+            </div>
+        </div>
+    `
+}
+
+function generateGallery(artworkArray) {
+    const container = document.querySelector(".gallery-container");
+
+    const html = artworkArray.map(artwork => artworkTemplate(artwork)).join('');
+
+    container.innerHTML = html;
+};
+
+function init() {
+    generateGallery(artwork);
+}
+
+init();
+
+
+/* GRID TOGGLE */
+const modeToggle = document.getElementById("mode-toggle");
+modeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("toggle");
+})
+
+
+/* FILTERS */
+const filterSelect = document.getElementById("filter");
+
+filterSelect.addEventListener("change", () => {
+    const filterArray = [];
+
+    if (filterSelect.value === "all") {
+        generateGallery(artwork);
+    } else {
+        artwork.forEach((art) => {
+            if (filterSelect.value.toLocaleLowerCase() === art.type.toLocaleLowerCase()) {
+                filterArray.push(art);
+            }
+        });
+        generateGallery(filterArray);
+    }
+});
